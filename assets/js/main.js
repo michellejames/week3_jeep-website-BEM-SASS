3f1c31c7aa818d1af62d927b01f76baed606301a
michellejames <shellyjames90@yahoo.com>
2017-04-24 11:24:11 -0400

    Jeep Website

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,54 +1,21 @@
 console.log("Hello World from main.js!");
 
+var $hero = document.querySelector('.hero');
+var bgImage = 2;
 
-
-var currentSlide = -1;
-
-var imageData = [
-
-	{
-		filename:"assets/img/feature-1-bg.jpg",
-		title:"donut1",
-		description:"pink donuts"
-	},
-
-	{
-		filename:"assets/img/feature-2-bg.jpg",
-		title:"donut2",
-		description:"5 donuts"
-	},
-			
-];
-
-var $slideshow = document.querySelector("#hero__slideshow");
-
-var makeSlide = function(){
-
-	var $slide = document.createElement("img");
-	$slide.setAttribute("src", imageData[currentSlide].filename);
-	$slideshow.appendChild($slide);
-}	
-
-var removeSlide = function(){
-	
-	while ($slideshow.children.length) {
-		$slideshow.removeChild($slideshow.children[0]);
-	}
-}
-
-
-var advancedSlideshow = function(){
-	removeSlide();
-	currentSlide++;
-
-	if (currentSlide > 2) {
-		currentSlide = 0;
+function changeImage() {
+	if (bgImage == 3 ) {
+		bgImage = 1;
 	}
 
-	makeSlide();
+	$hero.style.opacity = 0.7;
+	$hero.style.backgroundImage = "url(assets/img/feature-" + bgImage + "-bg.jpg)";
+	console.log($hero.style.backgroundImage)
+	$hero.style.opacity = 1;
+	bgImage++;
 }
 
-setInterval(advancedSlideshow, 2000);
+setInterval(changeImage, 4000);
 
 
 
